c47cfd2820baef7eab485dc7053c291298f2737e
Danny Almeida <danyelaalmeida1811@gmail.com>
2023-09-28 23:09:54 -0300

    Add files via upload

---
+++ mobile-navbar.js
@@ -0,0 +1,46 @@
+function createMobileNavbar(mobileMenu, navList, navLinks) {
+    const activeClass = "active";
+  
+  
+    function handleClick() {
+        const navListElement = document.querySelector(navList);
+        if (navListElement) {
+            navListElement.classList.toggle(activeClass);
+
+            const navLinksElements = document.querySelectorAll(navLinks);
+
+            navLinksElements.forEach((navLink, index) => {
+                if (navLink.style.animation) {
+                    navLink.style.animation = "";
+                } else {
+                    // Adicione uma animação personalizada para os links aqui
+                    navLink.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+        const mobileMenuElement = document.querySelector(mobileMenu);
+        if (mobileMenuElement) {
+                // Adicione uma classe para alternar entre o ícone do menu e o ícone "X"
+           mobileMenuElement.classList.toggle(activeClass);
+            }
+        }
+    }
+
+    function addClickEvent() {
+        const mobileMenuElement = document.querySelector(mobileMenu);
+        if (mobileMenuElement) {
+            mobileMenuElement.addEventListener("click", handleClick);
+        }
+    }
+
+    function init() {
+        addClickEvent();
+    }
+
+    return {
+        init
+    };
+}
+
+const mobileNavbar = createMobileNavbar(".mobile-menu", ".nav-list", ".nav-list li");
+mobileNavbar.init();
